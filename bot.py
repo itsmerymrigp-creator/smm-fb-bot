554e355ae9d2a53614e7486506f7ca66dfa9051a
itsmerymrigp-creator <itsmerymrigp@gmail.com>
2026-04-24 13:58:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,12 +31,14 @@ def is_owner(uid):
 def get_session():
     session = requests.Session()
     session.headers.update({
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-    })
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+})
     
-    r = session.get('https://mbasic.facebook.com/login/')
+    r = session.get('https://mbasic.facebook.com/')
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(r.text, 'html.parser')
     
